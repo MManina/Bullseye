cdff0647e0ecb03584b243cc7ac39361a888a5a0
MManina <meng.manina@gmail.com>
2022-11-04 18:03:23 +0100

    video 18 : activity structure done

--- app/src/main/java/com/tuto/bullseye/MainActivity.kt
+++ app/src/main/java/com/tuto/bullseye/MainActivity.kt
@@ -2,10 +2,53 @@ package com.tuto.bullseye
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import androidx.appcompat.app.AlertDialog
+import com.tuto.bullseye.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var sliderValue = 0
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.hitMeButton.setOnClickListener {
+            Log.i("Button Click Event", "You click the hit me button")
+            showResult()
+        }
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                sliderValue = progress
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
+    }
+
+    private fun showResult() {
+        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogMessage = getString(R.string.result_dialog_message, sliderValue)
+//        val dialogMessage = "The slider's value is $sliderValue"
+
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.setPositiveButton(R.string.result_dialog_button_text) {dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
     }
 }
